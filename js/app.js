0dd117944752e0973dc20de58836cd9b09a77cff
hguilla <hguilla@gmail.com>
2016-07-09 01:40:12 -0300

    Add project frame injection instead of link

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 (function () {$(document).on('ready', function () {
 
 $('#navigation a:first').tab('show');
+function showInIframe(link) {
+    var $iframe = $('<iframe src="' + link + '" width="100%" height="100%"></iframe>'),
+        $project = $('#project');
+    $project.html($iframe);
+    $project.tab('show');
+}
+
+document.hguilla = {
+    showInIframe: showInIframe
+};
 
 })})();
