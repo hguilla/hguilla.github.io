97cba650c0ea20bb3cf6a65ab950a530a57736a4
hguilla <hguilla@gmail.com>
2016-07-09 02:24:16 -0300

    Change iframe min-height to class style

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 $('#navigation a:first').tab('show');
 function showInIframe(link) {
-    var $iframe = $('<iframe src="' + link + '" width="100%" height="100%" min-height="100vh"></iframe>'),
+    var $iframe = $('<iframe src="' + link + '" width="100%" height="100%" class="full-vh"></iframe>'),
         $project = $('#project');
     $project.html($iframe);
     $('#navigation a[href="#project"]').tab('show');
